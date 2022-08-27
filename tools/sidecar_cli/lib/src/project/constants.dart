e54d8791df606baf009afe1aa12a0238627e9a6b
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-08-27 16:33:49 -0400

    refactor: Source code utilities pubspec overrides.

--- tools/sidecar_cli/lib/src/project/constants.dart
+++ tools/sidecar_cli/lib/src/project/constants.dart
@@ -32,18 +32,24 @@ final kPluginLoaderPath = p.join(
   'sidecar_analyzer_plugin',
   'tools',
   'analyzer_plugin',
-  'pubspec.yaml',
+  'pubspec_overrides.yaml',
 );
 
 String pluginLoaderYamlContentCreator(String projectPluginPath) => '''
-name: sidecar_analyzer_plugin_loader
-description: This pubspec determines the version of the analyzer plugin to load.
-version: 0.0.1
-publish_to: none
-
-environment:
-  sdk: ">=2.15.0 <3.0.0"
-dependencies:
+dependency_overrides:
   sidecar_analyzer_plugin:
     path: $projectPluginPath/sidecar_analyzer_plugin # code-generated
 ''';
+
+// String pluginLoaderYamlContentCreator(String projectPluginPath) => '''
+// name: sidecar_analyzer_plugin_loader
+// description: This pubspec determines the version of the analyzer plugin to load.
+// version: 0.0.1
+// publish_to: none
+
+// environment:
+//   sdk: ">=2.15.0 <3.0.0"
+// dependencies:
+//   sidecar_analyzer_plugin:
+//     path: $projectPluginPath/sidecar_analyzer_plugin # code-generated
+// ''';
