002dc205f37374dc62f43ce0d5cf6f7b493292ec
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-08-31 16:48:10 -0400

    refactor: Remove redundant root files.

--- lints/avoid_string_literals/example/avoid_string_literals_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:avoid_string_literals/avoid_string_literals.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lints/avoid_string_literals/lib/avoid_string_literals.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library avoid_string_literals;
-
-export 'src/avoid_string_literals_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
