9f6220e3c2291f6500239b780a3bdc2d23888524
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-10-03 13:45:27 -0400

    chore: Update example to use design system annotations.

--- examples/my_analyzed_codebase/lib/core/theme/icons.dart
+++ examples/my_analyzed_codebase/lib/core/theme/icons.dart
@@ -1,17 +1,13 @@
 // APPROVED ICONS
 
 import 'package:flutter/material.dart';
-import 'package:sidecar_annotations/sidecar_annotations.dart';
 import 'package:design_system_annotations/design_system_annotations.dart';
 
-final x = '';
-
-@SidecarInput(packageName: 'design_system_lints', configuration: {})
 class ProjectIcons {
   static const abc = Icons.stop;
 }
 
-@designSystem
+// @designSystem
 class ProjectIcons2 {
   static const abc = Icons.party_mode;
 }
