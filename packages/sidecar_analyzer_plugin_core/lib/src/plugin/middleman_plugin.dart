7c90687f0d0ac82a1ebe62848ed6efe570390d5c
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-10-05 19:30:33 -0400

    feat: StreamProvider to listen to analyzer server.

--- packages/sidecar_analyzer_plugin_core/lib/src/plugin/middleman_plugin.dart
+++ packages/sidecar_analyzer_plugin_core/lib/src/plugin/middleman_plugin.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:analyzer/dart/analysis/analysis_context.dart';
@@ -7,29 +6,20 @@ import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/instrumentation/instrumentation.dart';
-
 import 'package:analyzer_plugin/channel/channel.dart' as plugin;
 import 'package:analyzer_plugin/plugin/plugin.dart' as plugin;
 import 'package:analyzer_plugin/protocol/protocol.dart' as plugin;
-import 'package:analyzer_plugin/protocol/protocol_generated.dart' as plugin;
+import 'package:analyzer_plugin/protocol/protocol_constants.dart';
+import 'package:analyzer_plugin/protocol/protocol_generated.dart';
+import 'package:analyzer_plugin/src/protocol/protocol_internal.dart' as plugin;
 import 'package:analyzer_plugin/src/channel/isolate_channel.dart' as plugin;
-import 'package:cli_util/cli_util.dart';
-
-import 'package:hotreloader/hotreloader.dart';
+import 'package:path/path.dart' as p;
 import 'package:riverpod/riverpod.dart';
-import 'package:sidecar/sidecar.dart';
-import 'package:uuid/uuid.dart';
 
 import '../../sidecar_analyzer_plugin_core.dart';
-import '../application/rules/activated_rules_notifier.dart';
 import '../constants.dart';
 import '../context_services/context_services.dart';
-import '../services/analysis_context_collection_service/analysis_context_collection_service.dart';
 import '../services/log_delegate/log_delegate.dart';
-import '../services/project_configuration_service/providers.dart';
-import 'analyzer_mode.dart';
-import 'package:path/path.dart' as p;
-import '../utils/byte_store_ext.dart';
 
 final middlemanPluginProvider = Provider(MiddlemanPlugin.new);
 
@@ -43,6 +33,7 @@ class MiddlemanPlugin extends plugin.ServerPlugin {
         );
 
   HotReloader? _reloader;
+  late plugin.ServerIsolateChannel pluginChannel;
   final initializationCompleter = Completer<void>();
   final Ref _ref;
 
@@ -65,11 +56,23 @@ class MiddlemanPlugin extends plugin.ServerPlugin {
   void start(plugin.PluginCommunicationChannel channel) {
     delegate.sidecarMessage('MIDDLEMAN STARTING....');
     _start(channel);
-    // super.start(channel);
+    super.start(_ref.read(pluginChannelProvider));
   }
 
-  Future<void> _runProcess() async {
-    delegate.sidecarMessage('MM: _runProcess()');
+  @override
+  Future<void> afterNewContextCollection({
+    required AnalysisContextCollection contextCollection,
+  }) async {
+    //
+    for (final context in contextCollection.contexts) {
+      // if sidecar is enabled for context then start a plugin in that directory
+      delegate
+          .sidecarMessage('handling context: ${context.contextRoot.root.path}');
+    }
+  }
+
+  Future<void> _initializePlugin() async {
+    delegate.sidecarMessage('Middleman initialization started...');
     final root = p.join(
         Platform.environment['HOME']!,
         '.dartServer',
@@ -79,41 +82,30 @@ class MiddlemanPlugin extends plugin.ServerPlugin {
 
     final packagesPath = p.join(
         root, 'tools', 'analyzer_plugin', '.dart_tool', 'package_config.json');
+
     final executionPath =
         p.join(root, 'tools', 'analyzer_plugin', 'bin', 'sidecar.dart');
 
-    final chann = plugin.ServerIsolateChannel.discovered(
+    pluginChannel = plugin.ServerIsolateChannel.discovered(
       Uri.file(executionPath, windows: Platform.isWindows),
       Uri.file(packagesPath, windows: Platform.isWindows),
       NoopInstrumentationService(),
     );
-    await chann.listen(
-      (response) {
-        // delegate
-        //     .sidecarMessage('TESTRESPONSE: ${response.toJson().toString()}');
-        _ref.read(pluginChannelProvider).sendResponse(response);
-      },
-      (notification) {
-        // delegate.sidecarMessage(
-        //     'TESTNOTIFICATION: ${notification.toJson().toString()}');
-        _ref.read(pluginChannelProvider).sendNotification(notification);
-      },
-      onDone: () {
-        delegate.sidecarMessage('TESTDONE');
-      },
-      onError: (error) {
-        delegate.sidecarMessage('TESTERROR: ${error.toString()}');
-      },
+    await pluginChannel.listen(
+      _ref.read(pluginChannelProvider).sendResponse,
+      _ref.read(pluginChannelProvider).sendNotification,
+      onDone: () => delegate.sidecarMessage('DONE'),
+      onError: (error) => delegate.sidecarMessage('ERROR: ${error.toString()}'),
     );
 
-    _ref.read(pluginChannelProvider).listen(chann.sendRequest);
-    delegate.sidecarMessage('MM: _runProcess() COMPLETED');
+    _ref.read(pluginStreamProvider.stream).listen(_onRequest);
+    delegate.sidecarMessage('Middleman initialization completed.');
   }
 
   Future<void> _start(
     plugin.PluginCommunicationChannel channel,
   ) async {
-    await _runProcess();
+    await _initializePlugin();
     if (mode.isDebug) await _startWithHotReload(channel);
   }
 
@@ -138,8 +130,109 @@ class MiddlemanPlugin extends plugin.ServerPlugin {
     required AnalysisContext analysisContext,
     required String path,
   }) async {
-    // do nothing
-
     throw UnimplementedError();
   }
+
+  /// Compute the response that should be returned for the given [request], or
+  /// `null` if the response has already been sent.
+  Future<plugin.Response?> _getResponse(
+      plugin.Request request, int requestTime) async {
+    plugin.ResponseResult? result;
+    switch (request.method) {
+      // case ANALYSIS_REQUEST_GET_NAVIGATION:
+      //   var params = AnalysisGetNavigationParams.fromRequest(request);
+      //   result = await handleAnalysisGetNavigation(params);
+      //   break;
+      // case ANALYSIS_REQUEST_HANDLE_WATCH_EVENTS:
+      //   var params = AnalysisHandleWatchEventsParams.fromRequest(request);
+      //   result = await handleAnalysisHandleWatchEvents(params);
+      //   break;
+      case ANALYSIS_REQUEST_SET_CONTEXT_ROOTS:
+        final params = AnalysisSetContextRootsParams.fromRequest(request);
+        result = await handleAnalysisSetContextRoots(params);
+        break;
+      default:
+        pluginChannel.sendRequest(request);
+        break;
+      // case ANALYSIS_REQUEST_SET_PRIORITY_FILES:
+      //   var params = AnalysisSetPriorityFilesParams.fromRequest(request);
+      //   result = await handleAnalysisSetPriorityFiles(params);
+      //   break;
+      // case ANALYSIS_REQUEST_SET_SUBSCRIPTIONS:
+      //   var params = AnalysisSetSubscriptionsParams.fromRequest(request);
+      //   result = await handleAnalysisSetSubscriptions(params);
+      //   break;
+      // case ANALYSIS_REQUEST_UPDATE_CONTENT:
+      //   var params = AnalysisUpdateContentParams.fromRequest(request);
+      //   result = await handleAnalysisUpdateContent(params);
+      //   break;
+      // case COMPLETION_REQUEST_GET_SUGGESTIONS:
+      //   var params = CompletionGetSuggestionsParams.fromRequest(request);
+      //   result = await handleCompletionGetSuggestions(params);
+      //   break;
+      // case EDIT_REQUEST_GET_ASSISTS:
+      //   var params = EditGetAssistsParams.fromRequest(request);
+      //   result = await handleEditGetAssists(params);
+      //   break;
+      // case EDIT_REQUEST_GET_AVAILABLE_REFACTORINGS:
+      //   var params = EditGetAvailableRefactoringsParams.fromRequest(request);
+      //   result = await handleEditGetAvailableRefactorings(params);
+      //   break;
+      // case EDIT_REQUEST_GET_FIXES:
+      //   var params = EditGetFixesParams.fromRequest(request);
+      //   result = await handleEditGetFixes(params);
+      //   break;
+      // case EDIT_REQUEST_GET_REFACTORING:
+      //   var params = EditGetRefactoringParams.fromRequest(request);
+      //   result = await handleEditGetRefactoring(params);
+      //   break;
+      // case KYTHE_REQUEST_GET_KYTHE_ENTRIES:
+      //   var params = KytheGetKytheEntriesParams.fromRequest(request);
+      //   result = await handleKytheGetKytheEntries(params);
+      //   break;
+      // case PLUGIN_REQUEST_SHUTDOWN:
+      //   var params = PluginShutdownParams();
+      //   result = await handlePluginShutdown(params);
+      //   _channel.sendResponse(result.toResponse(request.id, requestTime));
+      //   _channel.close();
+      //   return null;
+      // case PLUGIN_REQUEST_VERSION_CHECK:
+      //   var params = PluginVersionCheckParams.fromRequest(request);
+      //   result = await handlePluginVersionCheck(params);
+      //   break;
+    }
+    if (result == null) {
+      return plugin.Response(request.id, requestTime,
+          error: plugin.RequestErrorFactory.unknownRequest(request.method));
+    }
+    return result.toResponse(request.id, requestTime);
+  }
+
+  bool _isPriorityAnalysisContext(AnalysisContext analysisContext) {
+    return priorityPaths.any(analysisContext.contextRoot.isAnalyzed);
+  }
+
+  /// The method that is called when a [request] is received from the analysis
+  /// server.
+  Future<void> _onRequest(plugin.Request request) async {
+    var requestTime = DateTime.now().millisecondsSinceEpoch;
+    var id = request.id;
+    plugin.Response? response;
+    try {
+      response = await _getResponse(request, requestTime);
+    } on plugin.RequestFailure catch (exception) {
+      response = plugin.Response(id, requestTime, error: exception.error);
+    } catch (exception, stackTrace) {
+      response = plugin.Response(id, requestTime,
+          error: RequestError(
+              RequestErrorCode.PLUGIN_ERROR, exception.toString(),
+              stackTrace: stackTrace.toString()));
+    }
+    if (response != null) {
+      _channel.sendResponse(response);
+    }
+  }
+
+  plugin.PluginCommunicationChannel get _channel =>
+      _ref.read(pluginChannelProvider);
 }
--- packages/sidecar_analyzer_plugin_core/lib/src/plugin/plugin_channel_provider.dart
+++ packages/sidecar_analyzer_plugin_core/lib/src/plugin/plugin_channel_provider.dart
@@ -1,5 +1,17 @@
+import 'dart:async';
+
 import 'package:analyzer_plugin/channel/channel.dart' as plugin;
+import 'package:analyzer_plugin/protocol/protocol.dart' as plugin;
 import 'package:riverpod/riverpod.dart';
 
 final pluginChannelProvider = Provider<plugin.PluginCommunicationChannel>(
     (ref) => throw UnimplementedError());
+
+final pluginStreamProvider = StreamProvider<plugin.Request>((ref) {
+  final controller = StreamController<plugin.Request>();
+
+  ref.read(pluginChannelProvider).listen(controller.add,
+      onDone: controller.close, onError: controller.close);
+
+  return controller.stream;
+});
