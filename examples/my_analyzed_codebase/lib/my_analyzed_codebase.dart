c16d008f0af337e28a89f43f36bf74414c8bdbfd
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-11-27 13:08:11 -0500

    chore: Update intl_lints to v0.1.0-dev.2.

--- examples/my_analyzed_codebase/lib/my_analyzed_codebase.dart
+++ examples/my_analyzed_codebase/lib/my_analyzed_codebase.dart
@@ -14,9 +14,7 @@ calculate() {
 class SomePage extends ConsumerWidget {
   final color = Color(0x12345678);
   final insets = EdgeInsets.all(10);
-  //
 
-  //
   final shadow = BoxShadow();
 
   @override
