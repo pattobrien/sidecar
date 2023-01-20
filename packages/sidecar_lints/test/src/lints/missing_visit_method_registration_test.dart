ed99d731d42b6a37d02cc583d564d8018c965283
pattobrien <12474318+pattobrien@users.noreply.github.com>
2023-01-20 11:17:06 -0500

    test: All methods are registered, enabling sidecar_lints ci/cd.

--- packages/sidecar_lints/test/src/lints/missing_visit_method_registration_test.dart
+++ packages/sidecar_lints/test/src/lints/missing_visit_method_registration_test.dart
@@ -6,11 +6,44 @@ void main() {
   group('missing_visit_method_registration: ', () {
     setUpRules([MissingVisitMethodRegistration()]);
 
-    ruleTest('missing annotation', content, [ExpectedText('visitAnnotation')]);
+    ruleTest('all methods are registered', content, []);
+
+    ruleTest(
+      'missing registration',
+      contentMissingRegistration,
+      [ExpectedText('visitAnnotation')],
+    );
   });
 }
 
 const content = '''
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:sidecar/sidecar.dart';
+
+class ExampleLint extends LintRule {
+  static const _id = 'example_lint';
+
+  @override
+  LintCode get code => LintCode(_id, package: 'example');
+
+  @override
+  void initializeVisitor(NodeRegistry registry) => registry
+    ..addAdjacentStrings(this)
+    ..addAnnotation(this);
+
+  @override
+  void visitAdjacentStrings(AdjacentStrings node) {
+    super.visitAdjacentStrings(node);
+  }
+
+  @override
+  void visitAnnotation(Annotation node) {
+    super.visitAnnotation(node);
+  }
+}
+''';
+
+const contentMissingRegistration = '''
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:sidecar/sidecar.dart';
 
