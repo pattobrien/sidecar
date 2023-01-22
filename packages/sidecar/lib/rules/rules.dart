f59a1583916bc14035b129648359b7e405a448f1
pattobrien <12474318+pattobrien@users.noreply.github.com>
2023-01-22 14:19:50 -0500

    refactor: Hide internal AstVisitor classes, extensions.

--- packages/sidecar/lib/rules/rules.dart
+++ packages/sidecar/lib/rules/rules.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: avoid_equals_and_hash_code_on_mutable_classes
-
 import '../src/rules/rules.dart';
 
 export '../src/rules/base_rule.dart' show QuickFix, SidecarBaseConstructor;
-export '../src/rules/lint_severity.dart';
+export '../src/rules/lint_severity.dart' show LintSeverity;
 
 /// Create a lint.
 abstract class LintRule = Rule with Lint;
@@ -12,6 +10,7 @@ abstract class LintRule = Rule with Lint;
 abstract class AssistRule = Rule with QuickAssist;
 
 /// Capture data.
+@Deprecated('')
 abstract class DataRule = Rule with Data;
 
 
--- packages/sidecar/lib/sidecar.dart
+++ packages/sidecar/lib/sidecar.dart
@@ -1,12 +1,17 @@
+// Temporarily ignore ordering
+// ignore_for_file: directives_ordering
+
 library sidecar;
 
+// is this needed any more?
 export 'package:analyzer/dart/analysis/results.dart' hide AnalysisResult;
 
 export 'rules/rules.dart';
-export 'src/analyzer/ast/ast.dart' hide VisitorSubscription;
 export 'src/configurations/exceptions/exceptions.dart';
 export 'src/protocol/analyzer_plugin_exts/analyzer_plugin_exts.dart';
 export 'src/protocol/protocol.dart' hide SidecarType;
 export 'src/utils/utils.dart';
 
+// Approved public APIs
+export 'src/analyzer/ast/ast.dart' show NodeRegistry;
 export 'type_checker.dart';
